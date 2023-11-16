45b9d4486ffb2d98a1103394bd055f7de3588355
Rick Huang <rhuang@evertz.com>
2023-11-16 14:12:20 -0500

    modify receiver main to use CarControllingAgent

--- bt_receiver/receiver.py
+++ bt_receiver/receiver.py
@@ -1,6 +1,7 @@
 import asyncio
-from bleak import BleakScanner, BleakClient, BLEDevice
-import voting
+
+from bleak import BleakClient, BleakScanner, BLEDevice
+from car_control import CarControllingAgent
 
 # TODO: Add comments for below
 service = "19B10000-E8F2-537E-4F6C-D104768A1214"
@@ -8,6 +9,10 @@ left_char_uuid = "19B10000-E8F2-537E-4F6C-D104768A1214"
 right_char_uuid = "19B10000-E8F2-537E-4F6C-D104768A1215"
 
 
+# Arduino is sending the EMG readings every 40 ms.
+# So, 25 groups of readings per second are received in here.
+# TODO: We may need to tune the activate threshold to have a good muscle activation detection.
+car_controlling_agent = CarControllingAgent(voting_num=25, activate_threshold=0.3)
 
 
 # TODO: We may want a new main which is not async and waiting for readings.
@@ -23,20 +28,24 @@ async def main():
     print(devices)
     for d in devices:
         print(d.details)
-        if d.details["props"].get("Name") == 'EMG':
+        if d.details["props"].get("Name") == "EMG":
             my_device = d
-            print('Found it')
+            print("Found it")
             break
 
     async with BleakClient(my_device) as client:
-        LeftBuffer, RightBuffer = [], []
         while True:
             left_reading = await client.read_gatt_char(left_char_uuid)
             print(f"Left Reading is {int.from_bytes(left_reading, byteorder='big')}")
             right_reading = await client.read_gatt_char(right_char_uuid)
             print(f"Right Reading is {int.from_bytes(right_reading, byteorder='big')}")
-            # vote
-            voting.fill(LeftBuffer, left_reading, RightBuffer, right_reading)
-            voting.vote(LeftBuffer, RightBuffer)
+
+            # voting and taking car action based on voting result
+            car_controlling_agent.fill_readings(
+                left_reading=left_reading, right_reading=right_reading
+            )
+            car_controlling_agent.vote()
+            car_controlling_agent.refresh_car_action()
+
 
 asyncio.run(main())
