d7b2e50ea9f18f6a7e72cf433fa9ef07edd1a8e5
Qian Tang <qianqian.tang@mail.utoronto.ca>
2023-11-28 15:10:28 -0500

    added calibration phase not test yet

--- bt_receiver/car_control.py
+++ bt_receiver/car_control.py
@@ -4,10 +4,10 @@ that decides and control the car action.
 """
 # import threading
 # import time
-
+from datetime import datetime
 
 from enum import Enum
-
+import numpy as np
 # import car
 
 
@@ -19,7 +19,9 @@ class MotorActions(Enum):
 
 
 class CarControllingAgent:
-    def __init__(self, voting_num=25, activate_threshold=0.3):
+    def __init__(self, voting_num=25, activate_threshold_left=0.5,activate_threshold_right=0.3):
+        self.data = []
+
         # number of votes for one round of voting
         self.voting_num = voting_num
         self.left_buffer = []
@@ -33,7 +35,8 @@ class CarControllingAgent:
         self.right_min = float("-inf")
         # EMG reading activate threshold, a percentage of muscle pressure
         # Muscle with EMG Reading beyond this threshold are considered as active.
-        self.activate_threshold = activate_threshold
+        self.activate_threshold_left = activate_threshold_left
+        self.activate_threshold_right = activate_threshold_right
 
     def reset_voting_buffer(self):
         self.voting_buffer_length = 0
@@ -41,6 +44,8 @@ class CarControllingAgent:
         self.right_buffer = []
 
     def fill_readings(self, left_reading, right_reading):
+        
+        self.data.append([left_reading,right_reading])              
         self.left_max = max(left_reading, self.left_max)
         self.left_min = min(left_reading, self.left_min)
         self.right_max = max(right_reading, self.right_max)
@@ -51,27 +56,73 @@ class CarControllingAgent:
         self.voting_buffer_length += 1
 
     def vote(self):
-        # not start a voting round due to insufficient number of votes
+        # # not start a voting round due to insufficient number of votes
+        # if self.voting_buffer_length < self.voting_num:
+        #     return
+
+        # # count votes
+        # left_diff = self.left_max - self.left_min
+        # right_diff = self.right_max - self.right_min
+        # left_count, right_count = 0, 0
+        # for i in range(self.voting_num):
+        #     left_change_percent = (self.left_buffer[i] - self.left_min) /  (left_diff +1e-323 )
+        #     if left_change_percent >= self.activate_threshold:
+        #         left_count += 1
+        #     else:
+        #         left_count -= 1
+        #     right_change_percent = (self.right_buffer[i] - self.right_min) / (right_diff+1e-323 )
+        #     if right_change_percent >= self.activate_threshold:
+        #         right_count += 1
+        #     else:
+        #         right_count -= 1
+
+        # # decide voting result
+        # if left_count > 0 and right_count > 0:
+        #     self.voting_result = MotorActions.FORWARD
+        # elif left_count > 0 and right_count < 0:
+        #     self.voting_result = MotorActions.TURNLEFT
+        # elif left_count < 0 and right_count > 0:
+        #     self.voting_result = MotorActions.TURNRIGHT
+        # else:
+        #     self.voting_result = MotorActions.STOP
+
+        # # finish one round of voting, and reset voting buffer
+        # self.reset_voting_buffer()
+        
+        # add on 11/27
         if self.voting_buffer_length < self.voting_num:
             return
 
-        # count votes
-        left_diff = self.left_max - self.left_min
-        right_diff = self.right_max - self.right_min
+        normalized_left_buffer = self.normalize_data(self.left_buffer)
+        normalized_right_buffer = self.normalize_data(self.right_buffer)
+        # print(normalized_left_buffer, datetime.now())
+        # print(normalized_right_buffer),datetime.now() 
         left_count, right_count = 0, 0
         for i in range(self.voting_num):
-            left_change_percent = (self.left_buffer[i] - self.left_min) / left_diff
-            if left_change_percent >= self.activate_threshold:
+            if normalized_left_buffer[i] >= self.activate_threshold_left:
                 left_count += 1
             else:
                 left_count -= 1
-            right_change_percent = (self.right_buffer[i] - self.right_min) / right_diff
-            if right_change_percent >= self.activate_threshold:
+
+            if normalized_right_buffer[i] >= self.activate_threshold_right:
                 right_count += 1
             else:
                 right_count -= 1
 
-        # decide voting result
+        self.decide_action(left_count, right_count)
+        # print(self.left_buffer)
+        # print(self.right_buffer)
+        self.reset_voting_buffer()
+        
+    # add on 11/27    
+    def normalize_data(self, buffer):
+        buffer_array = np.array(buffer)
+        normalized_buffer = (buffer_array - np.min(buffer_array)) / (np.ptp(buffer_array) + 1e-323)
+        return normalized_buffer.tolist()
+    
+    # add on 11/27
+    def decide_action(self, left_count, right_count):
+        
         if left_count > 0 and right_count > 0:
             self.voting_result = MotorActions.FORWARD
         elif left_count > 0 and right_count < 0:
@@ -81,32 +132,32 @@ class CarControllingAgent:
         else:
             self.voting_result = MotorActions.STOP
 
-        # finish one round of voting, and reset voting buffer
-        self.reset_voting_buffer()
 
     def refresh_car_action(self):
         """
         refresh car action based on the voting result, and send signal to control the car
         """
         if self.voting_result == MotorActions.FORWARD:
-            # print("forward")
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             # car.Motor_Forward()
             pass
         elif self.voting_result == MotorActions.TURNLEFT:
-            # print("left")
+            print("<<<<<<<<<<<<<<-------------")
             # car.Motor_TurnLeft()
             pass
         elif self.voting_result == MotorActions.TURNRIGHT:
-            # print("right")
+            print("--------------->>>>>>>>>>>>")
             # car.Motor_TurnRight()
             pass
+            
         else:
-            # print("back")
+            print("stop")
             # car.Motor_Stop()
             pass
+            
         # print(f"Voting result is {self.voting_result}")
 
-    def stop_car(self):
-        self.voting_result = MotorActions.STOP
-        # print("Stop")
-        # car.Motor_Stop()
+    # def stop_car(self):
+    #     self.voting_result = MotorActions.STOP
+    #     print("Stop")
+    #     # car.Motor_Stop()
--- bt_receiver/receiver.py
+++ bt_receiver/receiver.py
@@ -8,6 +8,11 @@ from car_control import CarControllingAgent
 # import daemon
 import threading
 import time
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
 # TODO: Add comments for below
 service = "19B10000-E8F2-537E-4F6C-D104768A1214"
 left_char_uuid = "19B10000-E8F2-537E-4F6C-D104768A1214"
@@ -16,20 +21,24 @@ right_char_uuid = "19B10000-E8F2-537E-4F6C-D104768A1215"
 # So, 25 groups of readings per second are received in here.
 # TODO: We may need to tune the activate threshold to have a good muscle activation detection.
 global car_controlling_agent
-car_controlling_agent = CarControllingAgent(voting_num=25, activate_threshold=0.3)
+car_controlling_agent = CarControllingAgent(voting_num=25)
 
+plt.ion()  # Interactive mode on for dynamic plot updates
+times = []
+left_hand_emg = []
+right_hand_emg = []
 
-# def do_something():
-#     while True:
-#         if signal_start:
-#             car_controlling_agent.refresh_car_action()
-#         else:
-#             car_controlling_agent.stop_car()
-
-# def init_daemon():
-#     with daemon.DaemonContext():
-#         do_something()
-
+def update(frame):
+    global times, left_hand_emg, right_hand_emg
+    # Update the plot
+    plt.cla()
+    plt.plot(times, left_hand_emg, label='Left Hand')
+    plt.plot(times, right_hand_emg, label='Right Hand')
+    plt.xlabel('Time (s)')
+    plt.ylabel('EMG Value')
+    plt.title('Real-time EMG Data')
+    plt.legend()
+        
 # Create an Event object to control the thread's behavior
 signal_start= threading.Event()
 
@@ -37,6 +46,7 @@ def do_something():
     global car_controlling_agent
     global signal_start
     while True:
+        time.sleep(1)
         if signal_start.is_set():
             car_controlling_agent.refresh_car_action()
         else:
@@ -75,24 +85,73 @@ async def main():
     #         my_device = d
     #         print("Found it")
     #         break
+    def calculate_thresholds(readings):
+        # Calculate the threshold as a percentage of the maximum value
+        # Adjust the percentage as needed
+        threshold_percentage = 0.5
+        max_reading = max(readings)
+        return max_reading * threshold_percentage
 
     async with BleakClient(my_device) as client:
         
+        # Calibration phase - collect data for 5 seconds
+        calibration_data_left = []
+        calibration_data_right = []
+        start_time = time.time()
+        while time.time() - start_time < 5:
+            left_reading = await client.read_gatt_char(left_char_uuid)
+            right_reading = await client.read_gatt_char(right_char_uuid)
+            left_reading = int.from_bytes(left_reading, byteorder='big')
+            right_reading = int.from_bytes(right_reading, byteorder='big')
+            calibration_data_left.append(left_reading)
+            calibration_data_right.append(right_reading)
+            
+            
+                # Determine thresholds based on calibration data
+        left_threshold = calculate_thresholds(calibration_data_left)
+        right_threshold = calculate_thresholds(calibration_data_right)
+        car_controlling_agent.activate_threshold_left = left_threshold
+        car_controlling_agent.activate_threshold_right = right_threshold
+        
+        
         while True:
+            
+
+            
             left_reading = await client.read_gatt_char(left_char_uuid)
             left_reading = int.from_bytes(left_reading, byteorder='big')
-            print(f"Left Reading is {left_reading}")
+            # print(f"Left Reading is {left_reading}.{datetime.now()}")
             right_reading = await client.read_gatt_char(right_char_uuid)
             right_reading = int.from_bytes(right_reading, byteorder='big')
-            print(f"Right Reading is {right_reading}")
-
+            # print(f"Right Reading is {right_reading}.{datetime.now()}")
             # voting and taking car action based on voting result
-            car_controlling_agent.fill_readings(
-                left_reading=left_reading, right_reading=right_reading
-            )
             
-            car_controlling_agent.vote()
-            car_controlling_agent.refresh_car_action()
+            if left_reading >= 120:
+                if right_reading >= 70:
+                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                else:
+                    print("<<<<<<<<<<<<<<-------------")
+            else:
+                if right_reading >= 70:
+                    print("--------------->>>>>>>>>>>>")
+                else:
+                    print("Stop")
+                    
+            # left_hand_emg.append(left_reading)
+            # right_hand_emg.append(right_hand_emg)
+            # times.append(datetime.now())
+            # ani = FuncAnimation(plt.gcf(), update, interval=100)
+            # plt.tight_layout()
+            # plt.show()
+            # car_controlling_agent.fill_readings(
+            #     left_reading=left_reading, right_reading=right_reading
+            # )
+            
+            # car_controlling_agent.vote()
+            
+            
+            
+            # car_controlling_agent.refresh_car_action()
 
 
 asyncio.run(main())
