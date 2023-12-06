8d18dfdf68c433ce8e58c00b12885f8b91a99cc2
Qian Tang <qianqian.tang@mail.utoronto.ca>
2023-12-05 22:01:41 -0500

    make change to swim

--- bt_receiver/test_swim.py
+++ bt_receiver/test_swim.py
@@ -1,44 +1,33 @@
-import time
-from rpi_ws281x import PixelStrip, Color
+import board
+import neopixel
 
-# LED strip configuration:
-LED_COUNT = 21         # Number of LED pixels.
-LED_PIN = 18           # GPIO pin connected to the pixels (18 uses PWM).
-# Other configuration variables as needed...
+# Configure the setup
+LED_COUNT = 21          # Number of LED pixels.
+LED_PIN = board.D18     # GPIO pin connected to the NeoPixels (must support PWM).
+ORDER = neopixel.GRB    # Pixel color channel order
 
-strip = PixelStrip(LED_COUNT, LED_PIN, 800000, 5, True, 255)
-strip.begin()
+# Create the NeoPixel object
+strip = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=0.5, auto_write=False, pixel_order=ORDER)
 
-def light_up_led(index, color):
-    if index < 0 or index >= LED_COUNT:
-        return
+def update_strip():
+    # First 10 LEDs in red
+    for i in range(10):
+        strip[i] = (255, 0, 0)  # Red color
 
-    # Turn off all LEDs
-    for i in range(LED_COUNT):
-        strip.setPixelColor(i, Color(0, 0, 0))
-    
-    # Set the middle LED to white
-    strip.setPixelColor(10, Color(255, 255, 255))
+    # Middle LED in white
+    strip[10] = (255, 255, 255)  # White color
 
-    # Light up the specified LED
-    strip.setPixelColor(index, color)
+    # Last 10 LEDs in green
+    for i in range(11, 21):
+        strip[i] = (0, 255, 0)  # Green color
+
     strip.show()
 
-# Example usage
 try:
     while True:
-        # Example data received (replace with your data logic)
-        data = 5  # Replace with actual data index
-
-        # Decide the color based on the LED index
-        color = Color(255, 0, 0) if data < 10 else Color(0, 255, 0)
-
-        # Call the function to light up the LED
-        light_up_led(data, color)
-        time.sleep(1)
+        update_strip()
 
 except KeyboardInterrupt:
-    # Turn off all LEDs
-    for i in range(LED_COUNT):
-        strip.setPixelColor(i, Color(0, 0, 0))
+    # Turn off all the LEDs on Ctrl+C
+    strip.fill((0, 0, 0))
     strip.show()
