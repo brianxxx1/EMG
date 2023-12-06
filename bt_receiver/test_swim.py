ef31e35984fd924e43b07516c3772d7e48f76b13
Qian Tang <qianqian.tang@mail.utoronto.ca>
2023-12-05 22:32:30 -0500

    rpi_ws281x

--- bt_receiver/test_swim.py
+++ bt_receiver/test_swim.py
@@ -1,32 +1,39 @@
-import board
-import neopixel
+import time
+from rpi_ws281x import PixelStrip, Color
 
-# Configuration
+# LED strip configuration:
 LED_COUNT = 21          # Number of LED pixels
-LED_PIN = 32    # GPIO 12 used for the NeoPixels
+LED_PIN = 18            # GPIO pin connected to the pixels (18 uses PWM)
+LED_FREQ_HZ = 800000    # LED signal frequency in hertz (usually 800kHz)
+LED_DMA = 10            # DMA channel to use for generating signal
+LED_BRIGHTNESS = 255    # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False      # True to invert the signal (when using NPN transistor level shift)
 
 # Create NeoPixel object
-strip = neopixel.NeoPixel(LED_PIN, LED_COUNT, brightness=0.5, auto_write=False)
+strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+strip.begin()
 
 def update_strip():
     # First 10 LEDs one color (e.g., Red)
     for i in range(10):
-        strip[i] = (255, 0, 0)  # Red
+        strip.setPixelColor(i, Color(255, 0, 0))  # Red
 
     # Middle LED another color (e.g., White)
-    strip[10] = (255, 255, 255)  # White
+    strip.setPixelColor(10, Color(255, 255, 255))  # White
 
     # Last 10 LEDs another color (e.g., Green)
     for i in range(11, 21):
-        strip[i] = (0, 255, 0)  # Green
+        strip.setPixelColor(i, Color(0, 255, 0))  # Green
 
     strip.show()
 
 try:
     while True:
         update_strip()
+        time.sleep(1)
 
 except KeyboardInterrupt:
     # Turn off all LEDs when Ctrl+C is pressed
-    strip.fill((0, 0, 0))
+    for i in range(LED_COUNT):
+        strip.setPixelColor(i, Color(0, 0, 0))
     strip.show()
